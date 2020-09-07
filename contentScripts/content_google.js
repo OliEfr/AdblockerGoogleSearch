0503d83610d13d3afa071c4e629eccf132917a4e
PatrickJS <github@gdi2290.com>
2020-09-07 08:25:07 -0700

    feat: remove bottom ads

--- contentScripts/content_google.js
+++ contentScripts/content_google.js
@@ -1,30 +1,42 @@
 //bad option
 /*document.addEventListener('DOMContentLoaded', function() {
-    if(document.getElementById('tads')) {
-        document.getElementById('tads').remove()
-        console.log('Removed Google Serach Result Ads!')
-    }
+  if(document.getElementById('tads')) {
+    document.getElementById('tads').remove()
+    console.log('Removed Google Search Result Ads!')
+  }
 })*/
 
 //good option
 var removedAds = false;
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
-            var node1 = mutation.target.querySelector("#tads");
-            if (node1) {
-                node1.parentNode.removeChild(node1);
-                removedAds = true;
-            }
+const observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
+      // top ads
+      const node1 = mutation.target.querySelector("#tads");
+      // right ads
+      const node2 = mutation.target.querySelector(".commercial-unit-desktop-rhs");
+      // footer ads
+      const frNodes = mutation.target.querySelectorAll(".ads-fr");
+      if (frNodes) {
+        Array.from(frNodes).forEach(el => el.remove())
+      }
 
-            if (removedAds) {
-                observer.disconnect();
-            }
-        }
-    });
+      if (node2) {
+        node2.remove();
+      }
+      if (node1) {
+        node1.parentNode.removeChild(node1);
+        removedAds = true;
+      }
+
+      if (removedAds) {
+        observer.disconnect();
+      }
+    }
+  });
 });
 
 observer.observe(document, {
-    childList: true,
-    subtree: true
+  childList: true,
+  subtree: true
 });
