56a729e26ae5bb29453223a3319fc2ce3d424b05
PatrickJS <github@gdi2290.com>
2020-09-07 13:30:25 -0700

    refactor: style

--- contentScripts/content_google.js
+++ contentScripts/content_google.js
@@ -7,9 +7,11 @@
 })*/
 
 //good option
-var removedAds = false;
-const observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
+let removedAds = false;
+let timer = null;
+
+const observer = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
     if (mutation.addedNodes && (mutation.addedNodes.length > 0)) {
       const node1 = mutation.target.querySelector('[aria-label="Ads"]')
       // top ads
@@ -19,24 +21,27 @@ const observer = new MutationObserver(function(mutations) {
       // footer ads
       const node4 = mutation.target.querySelector('.ads-fr');
 
-      if (node4) {
+      if (node4 && node4.parentNode) {
         node4.parentNode.removeChild(node4);
       }
 
-      if (node1) {
+      if (node1 && node1.parentNode) {
         node1.parentNode.removeChild(node1);
       }
 
-      if (node2) {
+      if (node2 && node2.parentNode) {
         node2.parentNode.removeChild(node2);
       }
-      if (node3) {
+      if (node3 && node3.parentNode) {
         node3.parentNode.removeChild(node3);
         removedAds = true;
       }
 
-      if (removedAds) {
-        observer.disconnect();
+      if (removedAds && !timer) {
+        timer = true;
+        setTimeout(() => {
+          observer.disconnect();
+        }, 1);
       }
     }
   });
